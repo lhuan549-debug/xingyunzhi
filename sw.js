f2926db38b4b562ce5cb105ac7175e9c83d02d85
lhuan549-debug <lhuan549@gmail.com>
2026-05-04 06:44:26 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('clover-v1').then((cache) => cache.addAll(['index.html', 'manifest.json']))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
